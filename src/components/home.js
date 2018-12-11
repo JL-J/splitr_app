6d17655e2d08d3f84b83b21f352700dd4f1108c4
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-11 17:45:34 +0000

    Render NameInput

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import SubmitButton from './submitButton'
-
+import NameInput from './nameInput'
 export default class Home extends React.Component {
 
   render() {
     return (
       <View>
         <Text>Hello World!</Text>
+        <NameInput />
         <SubmitButton />
       </View>
 
