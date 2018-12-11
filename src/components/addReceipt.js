c434ba84925a5d9d0d5494834bafc5a6e6d345f2
Raylene <smith.raylene@gmail.com>
2018-12-11 18:25:25 +0000

    Add test and basic AddReceipt page

---
+++ src/components/addReceipt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class AddReceipt extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Receipt Page!</Text>
+      </View>
+    )
+  }
+}
