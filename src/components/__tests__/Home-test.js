a94af54de487f82cd7d963ca0fd50604a2db3bd2
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-11 17:44:40 +0000

    Create unit test for TextInput

--- src/components/__tests__/Home-test.js
+++ src/components/__tests__/Home-test.js
@@ -3,13 +3,15 @@ import 'react-native';
 import React from 'react';
 import Home from '../home.js';
 import SubmitButton from '../submitButton.js'
+import NameInput from '../nameInput.js'
 
 import renderer from 'react-test-renderer';
 
 describe('<Home />', () => {
-  it('home renders correctly', () => {
+
+  it('home renders a text input box for names', () => {
     const snap = renderer.create(
-      <Home />
+      <NameInput />
       ).toJSON();
     expect(snap).toMatchSnapshot();
   });
@@ -17,6 +19,13 @@ describe('<Home />', () => {
   it('home renders a button', () => {
     const snap = renderer.create(
       <SubmitButton />
+      ).toJSON();
+    expect(snap).toMatchSnapshot();
+  });
+
+  it('home renders correctly', () => {
+    const snap = renderer.create(
+      <Home />
       ).toJSON();
     expect(snap).toMatchSnapshot();
   });
