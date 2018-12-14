88a26cf288511fe19d85211abfb846f2bd4784e2
Reka <rekapap28@gmail.com>
2018-12-14 11:52:41 +0000

    refactores Home page and AddDishes page is working

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Home from './components/home'
+import AddDishes from './components/addDishes';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="home" component={Home} title="Sp/itr" />
+        <Scene key="Home" component={Home} title="Sp/itr" />
+        <Scene key="AddDishes" component={AddDishes} title="Sp/itr" />
       </Scene>
     </Router>
   );
