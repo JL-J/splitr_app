7880bc272be686f0c8c1e2a48f80dd66072d550c
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-14 12:42:50 +0000

    Update file pathways
    Co-Authored-By: Bill Muxworthy <BillMux@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import React from 'react';
-import App from './src/App';
-import {name as appName} from './app.json';
+import App from './App';
+import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
