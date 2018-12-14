28f69986a7c716c8615cd74e90f98ad2062602c4
Reka <rekapap28@gmail.com>
2018-12-14 11:51:18 +0000

    adds priceInput component

---
+++ src/components/priceInput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+
+export default class PriceInput extends React.Component {
+
+
+  render() {
+    return (
+      <TextInput
+      onChangeText = { this.props.nameHandler }
+      style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+      placeholder="Price"
+      />
+    )
+  }
+}
