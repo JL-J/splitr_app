f57f9f5f7d48803e6341f1c74276e6112fd610b1
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-15 15:33:55 +0000

    Adds styles to text
    Co-Authored-By: Bill Muxworthy <BillMux@users.noreply.github.com>

--- components/listReceipt.js
+++ components/listReceipt.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const ListReceipt = (props) => {
     return (
       <TouchableOpacity>
         <View>
-          <Text>{ props.dishName }</Text>
-          <Text>£{ props.price }</Text>
+          <Text style={styles.alignLeft}>{ props.dishName }</Text>
+          <Text style={styles.alignRight}>£{ props.price }</Text>
         </View>
       </TouchableOpacity>
     );
 }
+
+var styles = StyleSheet.create({
+  alignLeft: {
+    textAlign: 'left',
+  },
+  alignRight: {
+    textAlign: 'right',
+  },
+});
 
 export default ListReceipt;
