49fde35db4930dbe41e02136626f2f11a2a6b1c6
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-15 13:57:28 +0000

    Connects third page to store, output error with state keys
    Co-Authored-By: Bill Muxworthy <BillMux@users.noreply.github.com>

--- screens/addPeopleToDishes.js
+++ screens/addPeopleToDishes.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import ListReceipt from '../components/listReceipt';
+import { connect } from 'react-redux';
 
-export default class AddPeopleToDishes extends React.Component {
+export class AddPeopleToDishes extends React.Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     dishes: ["chips £4"]
+  //   };
+  // }
+
+  namesOutput = () => {
+    console.log(this.props.dishes)
+    return (
+      <FlatList
+      data = { this.props.dishes }
+      keyExtractor = {( item, index) => index.toString()}
+      renderItem = { info => (
+        <ListReceipt
+          dishName={ info.item.value.dishName }
+          price={ info.item.value.dishPrice }
+          />
+      )}
+      />
+    )
+  }
 
   render() {
     return (
-
-    )
+      <View>
+        <View>
+          <Text>Who owes what?</Text>
+        </View>
+        <View>
+          <Text>{ this.props.dishes }</Text>
+        </View>
+      </View>
+    );
   }
+}
+
+const mapStateToProps = (state) => {
+  const { dishes } = state
+  return { dishes }
+};
+
+export default connect(mapStateToProps)(AddPeopleToDishes);
