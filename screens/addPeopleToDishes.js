9f4d360fbca7925ae3a8d484fa89ec3628a82c39
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-16 18:04:54 +0000

    Change screen to with native-base. Toggle view not cureently working

--- screens/addPeopleToDishes.js
+++ screens/addPeopleToDishes.js
@@ -1,53 +1,77 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import ListReceipt from '../components/listReceipt';
-import ListItem from '../components/listItem';
+import { View, FlatList, StyleSheet, ListView } from 'react-native';
+import { Container, Header, Content, Button, Icon, List, ListItem, Text, SwipeRow, Body, Title } from 'native-base'
 import { connect } from 'react-redux';
 
 export class AddPeopleToDishes extends React.Component {
 
-  dishOutput = () => {
-    return (
-      <FlatList
-      data = { this.props.dishes }
-      keyExtractor = {( item, index) => index.toString()}
-      renderItem = { info => (
-        <ListReceipt
-          dishName={ info.item.value.dishName }
-          price={ info.item.value.dishPrice }
-          />
-      )}
-      />
-    )
+  constructor(props) {
+    super(props);
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      basic: true,
+      showReceipt: false,
+    };
   }
 
-  namesOutput = () => {
-    return (
-      <FlatList
-      data = { this.props.peopleNames }
-      keyExtractor = {( item, index) => index.toString()}
-      renderItem = { info => (
-        <ListItem
-          personName={ info.item.value }
-        />
-      )}
-      />
-    )
-  }
+ _toggleShow = () => {
+   this.setState({showReceipt: !this.state.showReceipt})
+ }
+
+ renderReceipt = () => {
+   if (this.state.showReceipt) {
+     return (
+       <FlatList
+       data = { this.props.dishes }
+       keyExtractor = {( item, index) => index.toString()}
+       renderItem = { info => (
+          <ListItem>
+           <Text>{ info.item.value.dishName }  £{ info.item.value.dishPrice }</Text>
+          </ListItem>
+       )}
+       />
+     );
+    } else {
+       return null;
+    }
+ }
 
   render() {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     return (
-      <View>
-        <View>
-          <Text>Who owes what?</Text>
-        </View>
-        <View>
-          { this.namesOutput() }
-        </View>
-        <View>
-          { this.dishOutput() }
-        </View>
-      </View>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Who owes what?</Title>
+          </Body>
+        </Header>
+        <Content scrollEnabled={false}>
+          <FlatList
+          data = { this.props.peopleNames }
+          keyExtractor = {( item, index) => index.toString()}
+          renderItem = { info => <SwipeRow
+              leftOpenValue={75}
+              rightOpenValue={-75}
+              left={
+                <Button items onPress={() => this._toggleShow()} >
+                   <Icon active name='paper' style={{color: 'white'}}/>
+                </Button>
+              }
+              body={
+                <View>
+                  <Text style={{ paddingLeft: 100, fontWeight: 'bold' }}>{ info.item.value }</Text>
+                  <List>{ this.renderReceipt()}</List>
+                </View>
+              }
+              right={
+                <Button trash style={{backgroundColor: 'red'}} onPress={() => alert('Delete')}>
+                   <Icon active name="trash" style={{color: 'white'}}/>
+                </Button>
+              }
+            />}
+          />
+        </Content>
+      </Container>
     );
   }
 }
