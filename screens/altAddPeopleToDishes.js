32290f1e93a0e318c20c759c5529d72b846b6355
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-16 18:04:14 +0000

    Change screen to without native-base

--- screens/altAddPeopleToDishes.js
+++ screens/altAddPeopleToDishes.js
@@ -1,54 +1,63 @@
+
 import React from 'react';
-import { View, FlatList, StyleSheet, ListView } from 'react-native';
-import { Container, Header, Content, Button, Icon, List, ListItem, Text, SwipeRow, Body, Title } from 'native-base'
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import ListReceipt from '../components/listReceipt';
+import ListItem from '../components/listItem';
 import { connect } from 'react-redux';
 
 export class AddPeopleToDishes extends React.Component {
-
   constructor(props) {
-    super(props);
-    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-    this.state = {
-      basic: true,
-    };
+    super(props)
+    this.state = { selected: false }
+  }
+
+  dishOutput = () => {
+    return (
+      <FlatList
+      data = { this.props.dishes }
+      keyExtractor = {( item, index) => index.toString()}
+      renderItem = { info => (
+        <TouchableOpacity
+          style={[styles.item, this.state.selected &amp;&amp; styles.itemAlt]}
+          onPress={()=> this.setState({selected: !this.state.selected})}
+        >
+          <View>
+          <Text style={styles.alignLeft}>{ info.item.value.dishName }</Text>
+          <Text style={styles.alignRight}>{ info.item.value.dishPrice }</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+      />
+    )
+  }
+
+  namesOutput = () => {
+    return (
+      <FlatList
+      data = { this.props.peopleNames }
+      keyExtractor = {( item, index) => index.toString()}
+      renderItem = { info => (
+        <ListItem
+          personName={ info.item.value }
+        />
+      )}
+      />
+    )
   }
 
   render() {
-    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     return (
-      <Container>
-        <Header>
-          <Body>
-            <Title>Who owes what?</Title>
-          </Body>
-        </Header>
-        <Content scrollEnabled={false}>
-          <FlatList
-          data = { this.props.peopleNames }
-          keyExtractor = {( item, index) => index.toString()}
-          renderItem = { info => <SwipeRow
-              leftOpenValue={75}
-              rightOpenValue={-75}
-              left={
-                <Button style={{backgroundColor: 'red'}} trash onPress={() => alert('Delete')} >
-                   <Icon active name="trash" style={{color: 'white'}}/>
-                </Button>
-              }
-              body={
-                <View>
-                  <Text style={{ paddingLeft: 15 }}>{ info.item.value }</Text>
-                </View>
-              }
-              right={
-                <Button items onPress={() => alert('Receipt')} >
-                   <Icon active name='paper' style={{color: 'white'}}/>
-                </Button>
-              }
-            />}
-          />
-        </Content>
-      </Container>
+      <View>
+        <View>
+          <Text>Who owes what?</Text>
+        </View>
+        <View>
+          { this.namesOutput() }
+        </View>
+        <View>
+          { this.dishOutput() }
+        </View>
+      </View>
     );
   }
 }
@@ -61,6 +70,18 @@ const mapStateToProps = state => {
 }
 
 var styles = StyleSheet.create({
-})
+  alignLeft: {
+    textAlign: 'left',
+  },
+  alignRight: {
+    textAlign: 'right',
+  },
+  item: {
+    backgroundColor: 'white',
+  },
+  itemAlt: {
+    backgroundColor: 'red',
+  },
+});
 
 export default connect(mapStateToProps)(AddPeopleToDishes);
