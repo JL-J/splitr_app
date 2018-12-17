83b70652edfe62520a9283fdb1cdfc8d5f1a7506
Reka <rekapap28@gmail.com>
2018-12-17 14:29:54 +0000

    adds trip reducer to combine reducer in store.js

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import nameReducer from './reducers/nameReducer';
 import dishReducer from './reducers/dishReducer';
+import tripReducer from './reducers/tripReducer';
 
 const rootReducer = combineReducers({
 	peopleNames: nameReducer,
-	dishes: dishReducer
+	dishes: dishReducer,
+	trips: tripReducer
 });
 
 const configureStore = () => {
