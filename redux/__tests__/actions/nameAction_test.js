f6c7b38765773f21d1353b42c43d9d3dd93d8f5a
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-12-19 17:15:14 +0000

    Adds action and reducer folders to tests folder

---
+++ redux/__tests__/actions/nameAction_test.js
@@ -0,0 +1,2 @@
+import configureStore from 'redux-mock-store';
+import * as selectActions from '../../actions/name';
---
+++ redux/__tests__/reducers/tripReducer_test.js
@@ -0,0 +1,50 @@
+import tripReducer from '../reducers/tripReducer'
+import configureStore from '../store';
+import * as actions from '../actions/trip';
+
+describe('tripReducer', () => {
+  it('returns initial state', () => {
+    expect(tripReducer(undefined, {})).toEqual({
+      currentTrip: {},
+      trips: []
+    });
+  });
+
+  it('adds trips', () => {
+    expect(tripReducer({
+      currentTrip: {},
+      trips: []
+    }, {
+      type: 'ADD_TRIP',
+      payload: ['Spain 2019']
+    })).toEqual({
+      currentTrip: {
+        "0": "Spain 2019"
+      },
+      trips: [{
+        'key': 0,
+        'value': ['Spain 2019']
+      }]
+    });
+  });
+
+  it('adds multiple trips', () => {
+    expect(tripReducer({
+      currentTrip: {},
+      trips: ['Spain 2019']
+    }, {
+      type: 'ADD_TRIP',
+      payload: ['Ski trip']
+    })).toEqual({
+      currentTrip: {
+        "0": "Ski trip"
+      },
+      trips: [
+        'Spain 2019', {
+          'key': 1,
+          'value': ['Ski trip']
+        }
+      ]
+    });
+  });
+});
