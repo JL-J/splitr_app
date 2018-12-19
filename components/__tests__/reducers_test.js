45b9ddaf73b216147fc21eabd5618b44b8f9b453
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-19 14:02:52 +0000

    WIP test
    Co-Authored-By: Bill Muxworthy <BillMux@users.noreply.github.com>

---
+++ components/__tests__/reducers_test.js
@@ -0,0 +1,9 @@
+import configureStore from '../../redux/store';
+import * from '../../redux/reducers';
+import * as actions from '../../redux/actions';
+
+describe('nameReducer', () => {
+  it('should return the initial stats', () => {
+    expect(reducer)
+  })
+})
