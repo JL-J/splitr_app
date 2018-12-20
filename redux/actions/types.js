387ef9930237fa065aa00ed875dad67786853563
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-20 12:36:46 +0000

    Correct redux type from dish to task

--- redux/actions/types.js
+++ redux/actions/types.js
@@ -1,5 +1,5 @@
 export const ADD_NAME = 'ADD_NAME';
 export const ADD_TASK = 'ADD_TASK';
 export const ADD_TRIP= 'ADD_TRIP';
-export const SELECT_NAME_TO_DISH = 'SELECT_NAME_TO_DISH';
+export const SELECT_NAME_TO_TASK = 'SELECT_NAME_TO_TASK';
 export const ASSIGN_TASKS = 'ASSIGN_TASKS';
