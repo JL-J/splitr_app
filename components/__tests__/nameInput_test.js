47ece6187b5c226f0ad58243a61ec40b54ef595b
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-12-21 12:29:01 +0000

    Tests new components
    Co-Authored-By: Bill Muxworthy <BillMux@users.noreply.github.com>

--- components/__tests__/nameInput_test.js
+++ components/__tests__/nameInput_test.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { TextInput } from 'react-native';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
-import NameInput from '../nameInput';
+import Input from '../input';
 
 describe('<NameInput />', function() {
   it('renders the component correctly', () => {
     const snap = renderer.create(
-      <NameInput />).toJSON();
+      <Input />).toJSON();
     expect(snap).toMatchSnapshot();
   });
 
   it('renders one text input component', () => {
-    const wrapper = shallow(<NameInput />);
+    const wrapper = shallow(<Input />);
     expect(wrapper.find(TextInput)).toHaveLength(1);
   });
 
